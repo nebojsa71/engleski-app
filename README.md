7ed25c0ec7b27a13c6bfdea5982662c818137067
nebojsa71 <31482856+nebojsa71@users.noreply.github.com>
2025-06-28 11:40:13 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Engleski za putovanja
+
+Web aplikacija za učenje engleskog jezika kroz fraze, izgovor i kvizove. Kompatibilna sa PWA standardom i pogodna za instalaciju na telefon.
---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const cacheName = 'engleski-app-v1';
+const assets = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
